f2aa737029ef4ea68e218eb4db97b999d5de64b9
Rainer Weston <rainerweston@gmail.com>
2020-11-05 16:06:27 +1300

    fix(debug): added FPS and system memory to debug overlay

--- src/scenes/debug_overlay.gd
+++ src/scenes/debug_overlay.gd
@@ -12,6 +12,14 @@ func _process(delta):
 	var label_text = "Debug \n"
 	label_text += "===============\n"
 	
+	var fps = Engine.get_frames_per_second()
+	label_text += str("FPS: ", fps)
+	label_text += "\n"
+	
+	var mem = OS.get_static_memory_usage()
+	label_text += str("Static Memory: ", String.humanize_size(mem))
+	label_text += "\n"
+	
 	for s in stats:
 		var value = null
 		#check if object not null, get a weakref to make sure object not freed
