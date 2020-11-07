99527463b8a75d40f10678ecf6a5dafacad9bb2b
Lucas K Allmon <lucaskallmon@gmail.com>
2020-11-07 14:07:17 -0800

    feature(exhaust): add particles when boosting

--- src/characters/player/Dropship.gd
+++ src/characters/player/Dropship.gd
@@ -1,28 +1,28 @@
-extends RigidBody2D
-
+extends KinematicBody2D
 export (int) var speed = 200
 export (int) var boost = 400
-signal ship_boosting(value)
 
 var velocity = Vector2()
-var rotation_dir = 0
 
 func get_input():
+	toggle_exhaust(false)
 	look_at(get_global_mouse_position())
-	#TODO: when the tip of the ship hits the mouse pointer, stop the ship.
+	#BUG: when the tip of the ship hits the mouse pointer, it freaks out.
 	velocity = Vector2(speed, 0).rotated(rotation)
 	
 	if Input.is_mouse_button_pressed(1):
-		#TODO: When mouse button clicked, give short burst of speed.
-		emit_signal("ship_boosting", true)
+		#TODO: Boost should be only for a second.
 		velocity = Vector2(speed + boost, 0).rotated(rotation)
-
-# Called when the node enters the scene tree for the first time.
+		toggle_exhaust(true)
+		
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _physics_process(_delta):
 	get_input()
 	rotation = get_global_mouse_position().angle_to_point(position)
-#	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity)
 
+func toggle_exhaust(value: bool):
+	get_node("particle-exhaust-1").emitting = value
+	get_node("particle-exhaust-2").emitting = value
