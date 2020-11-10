d0f3fe83644aa3f3ba7fc937fb3faeb0ba06223b
Lucas K Allmon <lucaskallmon@gmail.com>
2020-11-10 11:07:24 -0800

    chore(level): move test to main

--- src/Objects/Exit/Exit.gd
+++ src/Objects/Exit/Exit.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 func trigger_exit():
-	get_tree().change_scene("res://levels/test/test_level.tscn")
+	get_tree().change_scene("res://levels/main/Main.tscn")
 
 func _on_Exit_body_entered(body: Node) -> void:
 	trigger_exit()
