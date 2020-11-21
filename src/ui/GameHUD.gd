396f3eff42602335f4a475bd9fb68fef60fca296
Rainer Weston <rainerweston@gmail.com>
2020-11-21 21:58:36 +1300

    feat (GUI): added oxygen GUI element with update signal

--- src/ui/GameHUD.gd
+++ src/ui/GameHUD.gd
@@ -11,6 +11,7 @@ func subscribe_to_player():
 		
 	game.player.connect("depth_status", self, "_on_Sub_depth_status")
 	game.player.connect("hull_status", self, "_on_Sub_hull_status")
+	game.player.connect("oxygen_status", self, "_on_Sub_oxygen_status")
 	game.player.emit_signal("hull_status", game.player.hull_integrity)
 	
 func _on_Sub_depth_status(depth : float):
@@ -23,3 +24,11 @@ func _on_Sub_depth_status(depth : float):
 
 func _on_Sub_hull_status(integrity: int):
 	$HullIntegrity/Value.text = ("%d" % integrity)
+
+func _on_Sub_oxygen_status(oxygen:float):
+	print ("oxygen_status")
+	$Oxygen/Value.text = ("%d" % oxygen)
+
+func _on_Sub_destroyed():
+	pass
+	#self.visible = false
