94de2e9a8b7fa38eb7f171ff9bdb7a07ee301ee3
Rainer Weston <rainerweston@gmail.com>
2020-11-25 18:05:15 +1300

    fix (objects): Modified player spawn splash

--- src/Objects/SpawnSplash/SpawnSplash.gd
+++ src/Objects/SpawnSplash/SpawnSplash.gd
@@ -3,8 +3,12 @@ extends Area2D
 onready var energy = $Energy
 onready var bubbles = $Bubbles
 
+var triggered = false
+
 func _on_SpawnSplash_body_entered(body: Node) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and triggered == false:
+		triggered = true
 		energy.emitting = true
 		bubbles.emitting = true
+		$AudioStreamPlayer.play()
 		
